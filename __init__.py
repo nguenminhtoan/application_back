b7dc1c1d8c7787495f85c00b85aa8189f024ab00
グエン・ミン トアン <C_GuenMinToan@inside.sis.seino.co.jp>
2023-10-08 00:11:07 +0900

    run cicd 2

--- __init__.py
+++ __init__.py
@@ -6,11 +6,11 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
-from .movies/movies_api import movies_api
+#from .movies import movies_api
 
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(movies_api)
+#app.register_blueprint(movies_api)
 
 # Start app
 if __name__ == "__main__":
